b1b529e35f8534784375571f199d8d1b13f3b686
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-10-04 20:06:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Kushalhk/Autofilter.git /Autofilter 
+  git clone https://github.com/Kushalhk/TG_BOTZ.git /TG_BOTZ 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Autofilter 
+  git clone $UPSTREAM_REPO /TG_BOTZ
 fi
-cd /Autofilter 
+cd /TG_BOTZ 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
