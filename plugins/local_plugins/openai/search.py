aa2d372f50309f375832422b0fb8ace47bcbf936
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-10-05 19:04:35 +0530

    Create search.py

---
+++ plugins/local_plugins/openai/search.py
@@ -0,0 +1,11 @@
+#@KUSHALHK Thank me later "please don't remove my credits".
+from pyrogram import Client, filters
+from plugins.helper.engine import ask_ai
+
+
+@Client.on_message(filters.command('openai'))
+async def openai_ask(client, message):
+    if len(message.command) == 1:
+       return await message.reply_text("Give an input!")
+    m = await message.reply_text("👀")
+    await ask_ai(client, m, message)
