a19f61bc281bdb14471f3a3c0b1328146a7630bd
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-10-08 19:56:31 +0530

    Create wikipedia.py

---
+++ plugins/Extra/wikipedia.py
@@ -0,0 +1,29 @@
+import wikipedia
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command('wikipedia'))
+async def wiki(Client, message):
+    lang = message.command[1]
+    user_request = " ".join(message.command[2:])
+    if user_request == "":
+        wikipedia.set_lang("kan")
+        user_request = " ".join(message.command[1:])
+    try:
+        if lang == "ml":
+            wikipedia.set_lang("kan")
+
+        result = wikipedia.summary(user_request)
+        await message.edit(
+            f"""<b>Request:</b>
+<code>{user_request}</code>
+<b>Result:</b>
+<code>{result}</code>"""
+        )
+
+    except Exception as exc:
+        await message.edit(
+            f"""<b>Request:</b>
+<code>{user_request}</code>
+<b>Result:</b>
+<code>{exc}</code>"""
+        )
