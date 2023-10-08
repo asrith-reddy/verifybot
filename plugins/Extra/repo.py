c9ce33ffe0030df05a19ea92c45848a786c999ee
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-10-08 15:36:28 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -31,7 +31,7 @@ async def git(Kashmira, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powed by :</b> @lallu_tgs
+<b>Powed by :</b> @TG_UPDATES1
 """
         if qw.get("description"):
             txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
